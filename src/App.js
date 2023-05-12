d01387cdf87d88793b64332c25437c4c3cd72439
Rafael <rafayel.agazade567@gmail.com>
2023-05-12 15:11:13 +0400

    adding

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = { loading: false };
+
+    render() {
+        return <div className="App" />;
+    }
+}
+
+export default App;
